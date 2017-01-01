d95577f9c98b356377d4736b235656825626737a
Oussema Harbi <oussema.harbi@altran.com>
2017-01-01 18:43:49 +0100

    Remove unneded pro file

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -13,12 +13,12 @@ MainWindow::MainWindow(QWidget *parent) :
 
     centralWidget = new QWidget(this);
     centralWidget->setBaseSize(800,480);
-    central_frame = new InfoFrame(centralWidget);
+    /*central_frame = new InfoFrame(centralWidget);
 
     QVBoxLayout *layout = new QVBoxLayout;
     layout->addWidget(central_frame);
     centralWidget->setLayout(layout);
-
+    */
     this->setCentralWidget(centralWidget);
 
     centralWidget->show();
